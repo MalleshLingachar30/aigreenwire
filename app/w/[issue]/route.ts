b6cf8f330a6cf25262b0a8ae2a7c4edaf3621011
Mallesh <mallesh.lingachar@gmail.com>
2026-04-27 16:09:02 +0530

    Support share-versioned hub URLs

--- app/w/[issue]/route.ts
+++ app/w/[issue]/route.ts
@@ -1,6 +1,4 @@
-import {
-  parseIssueNumber,
-} from "@/lib/cards-language-reader";
+import { parseIssueNumber } from "@/lib/cards-language-reader";
 import { renderIssueHubResponse } from "@/lib/cards-issue-hub";
 
 type RouteParams = {
@@ -13,20 +11,27 @@ export async function GET(
 ) {
   const { issue } = await context.params;
   const issueNumber = parseIssueNumber(issue);
+  const requestUrl = new URL(request.url);
+  const shareVersion = requestUrl.searchParams.get("share")?.trim();
 
   if (!issueNumber) {
     return new Response("Invalid WhatsApp issue URL.", { status: 404 });
   }
 
-  const shortUrl = new URL(`/w/${issueNumber}`, request.url).toString();
-  const ogImageUrl = new URL(`/w/${issueNumber}/share-image?v=3`, request.url).toString();
+  const shortUrl = new URL(`/w/${issueNumber}`, request.url);
+  if (shareVersion) {
+    shortUrl.searchParams.set("share", shareVersion);
+  }
+
+  const ogImageUrl = new URL(`/w/${issueNumber}/share-image`, request.url);
+  ogImageUrl.searchParams.set("v", shareVersion || "3");
 
   return renderIssueHubResponse({
     issueNumber,
     shareMeta: {
-      canonicalUrl: shortUrl,
-      pageUrl: shortUrl,
-      ogImageUrl,
+      canonicalUrl: shortUrl.toString(),
+      pageUrl: shortUrl.toString(),
+      ogImageUrl: ogImageUrl.toString(),
     },
   });
 }
